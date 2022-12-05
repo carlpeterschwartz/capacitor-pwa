d446ae8e7990afbd7c957446793a67c3944ecf3f
Carl <github@carlpeterschwartz.com>
2022-12-05 21:21:41 +0100

    Initial commit.

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { Geolocation, Position } from '@capacitor/geolocation';
+import { Share } from '@capacitor/share';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  //myImage = null;
+  //myImage = '';
+
+  //myImage: string = null;
+  //position: Position = null;
+  //In tsconfig.json file, I added "strictNullChecks": false to accept null values below
+
+  myImage: string = null;
+  position: Position = null;
+
+  constructor() {}
+
+  async takePicture() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera
+    });
+
+    //this.myImage  = image.webPath;
+    if (image.webPath !== undefined){
+      this.myImage = image.webPath;
+    }
+  }
+
+  async getCurrentPosition() {
+    const coordinates = await Geolocation.getCurrentPosition();
+
+    this.position = coordinates;
+  }
+
+  async share() {
+    await Share.share({
+      title: 'Come and find me',
+      text: `Here's my current location:
+        ${this.position.coords.latitude},
+        ${this.position.coords.longitude}`,
+      url: 'http://ionicacademy.com/'
+    });
+  }
+
+}
